0e7f6562419c1dafec20f1423a36fc5df43d412f
z1nl0x <paulo.kreft@gmail.com>
2025-05-12 21:42:44 -0300

    Project updates

--- t_store/lib/utils/constants/image_strings.dart
+++ t_store/lib/utils/constants/image_strings.dart
@@ -1,8 +1,8 @@
 
 class TImages {
   // Logos do Aplicativo
-  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
-  static const String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+  static const String darkAppLogo = "assets/logos/kanji-black.png";
+  static const String lightAppLogo = "assets/logos/kanji-white.png";
 
   // Logos Socials
   static const String google = "assets/logos/google-icon.png";
