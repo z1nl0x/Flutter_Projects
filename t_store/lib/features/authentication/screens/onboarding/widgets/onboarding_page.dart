1cd46d338e5a6bf50473b4f6af0329c2024b4531
z1nl0x <paulo.kreft@gmail.com>
2025-05-22 21:46:10 -0300

    Project updates

--- t_store/lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
+++ t_store/lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/helpers/helper_functions.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 class OnBoardingPage extends StatelessWidget {
